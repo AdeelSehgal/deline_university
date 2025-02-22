99b3a92e30d6614665e47cc6f807eaf07a585229
adeel <adeel.sehgal@ovada.com>
2025-02-22 16:20:31 +0500

    correct folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { CourseComponent } from './components/course/course.component';
 import { AuthGuard } from './guard/auth.guard';
-import { ErrorPageComponent } from './error-page/error-page.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
 import { AddCourseVideoComponent } from './components/add-course-video/add-course-video.component';
 import { AddCourseComponent } from './components/add-course/add-course.component';
 import { AllAddedCoursesComponent } from './components/all-added-courses/all-added-courses.component';
