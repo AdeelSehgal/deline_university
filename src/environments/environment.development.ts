c24607fc7fa8fc88ea8901b0b09fc3de7f458296
Adeel <adeel.sehgal@ovada.com>
2025-03-04 11:46:07 +0500

    change enviroment variable

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    url: 'http://localhost:8080'
+    // url: 'http://localhost:8080'  // node js backend
+    url: 'http://127.0.0.1:8000'  // laravel backend
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    url: 'http://localhost:8080'
-    // url: 'http://127.0.0.1:8000/'
+    // url: 'http://localhost:8080'  // node js backend
+    url: 'http://127.0.0.1:8000'  // laravel backend
 };
